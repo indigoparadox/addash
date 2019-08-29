603284eb18828317f390cf39dc78db818069a16c
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-08-29 16:53:01 -0400

    Broke out remoteAD functions into their own module.

--- ADDash.ps1
+++ ADDash.ps1
@@ -1,53 +1,16 @@
 ﻿#
 # ADDash.ps1
 # A convenient set of tools for doing common tasks with the Active Directory.
+# Last Updated: indigoparadox, 2019/07/26
+#
+# - Split common remote DC functions out into RemoteAD library.
 #
 
 $LastSelectedIndex = 0
 
 $MyDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 Import-Module $MyDir\FormWrappers.ps1
-
-Function Invoke-OnDC {
-    Param(
-        [Parameter( Mandatory=$true, Position=0 )]
-        [System.Management.Automation.PSCredential] $AdminCredential,
-        [Parameter( Mandatory=$true, Position=1 )]
-        [scriptblock] $ScriptBlock
-    )
-    
-    $DomainController = Get-Registry -Hive HKCU -Path 'Software\ADDash' -Name 'DomainController'
-
-    Return $(Invoke-Command -Credential $AdminCredential -ComputerName $DomainController `
-        -ScriptBlock $ScriptBlock)
-}
-
-Function Get-RemoteADObject {
-    Param(
-        [Parameter( Mandatory=$true )]
-        [string] $OU,
-        [Parameter( Mandatory=$true )]
-        [string] $ObjectType,
-        [Parameter( Mandatory=$true )]
-        [string] $Filter,
-        [System.Management.Automation.PSCredential] $AdminCredential
-    )
-
-    $Properties = "DistinguishedName","Enabled","Name","SamAccountName","SID","LockedOut"
-
-    If( $ObjectType -eq 'User' ) {
-	    $ADDObjects = Invoke-OnDC $AdminCredential { Get-ADUser -SearchBase $Using:OU -Filter $Using:Filter -Properties $Using:Properties } |
-            Sort-Object -Property Name
-    } ElseIf( $ObjectType -eq 'Computer' ) {
-	    $ADDObjects = Invoke-OnDC $AdminCredential { Get-ADComputer -SearchBase $Using:OU -Filter $Using:Filter -Properties $Using:Properties } | 
-            Sort-Object -Property Name
-    } ElseIf( $ObjectType -eq 'Group' ) {
-	    $ADDObjects = Invoke-OnDC $AdminCredential { Get-ADGroup -SearchBase $Using:OU -Filter $Using:Filter } |
-            Sort-Object -Property Name
-    }
-
-    Return ,$ADDObjects
-}
+Import-Module $MyDir\RemoteAD.ps1
 
 Function Applet-Options {
     Param(
---
+++ RemoteAD.ps1
@@ -0,0 +1,53 @@
+﻿#
+# RemoteAD.ps1
+# Some utility functions for working with the domain controller remotely.
+# Requires admin credentials.
+# Last Updated: indigoparadox, 2019/07/26
+#
+
+Function Invoke-OnDC {
+    Param(
+        [Parameter( Mandatory=$true, Position=0 )]
+        [System.Management.Automation.PSCredential] $AdminCredential,
+        [Parameter( Mandatory=$true, Position=1 )]
+        [scriptblock] $ScriptBlock
+    )
+    
+    $DomainController = Get-Registry -Hive HKCU -Path 'Software\ADDash' -Name 'DomainController'
+
+    Return $(Invoke-Command -Credential $AdminCredential -ComputerName $DomainController `
+        -ScriptBlock $ScriptBlock)
+}
+
+Function Get-RemoteADObject {
+    Param(
+        [string] $OU,
+        [Parameter( Mandatory=$true )]
+        [string] $ObjectType,
+        [string] $Filter,
+        [string] $GUID,
+        [string] $RegistryPath,
+        [System.Management.Automation.PSCredential] $AdminCredential
+    )
+
+    $Properties = "DistinguishedName","Enabled","Name","SamAccountName","SID","LockedOut"
+
+    If( $ObjectType -eq 'User' ) {
+	    $ADDObjects = Invoke-OnDC $AdminCredential { Get-ADUser -SearchBase $Using:OU -Filter $Using:Filter -Properties $Using:Properties } |
+            Sort-Object -Property Name
+    } ElseIf( $ObjectType -eq 'Computer' ) {
+	    $ADDObjects = Invoke-OnDC $AdminCredential { Get-ADComputer -SearchBase $Using:OU -Filter $Using:Filter -Properties $Using:Properties } | 
+            Sort-Object -Property Name
+    } ElseIf( $ObjectType -eq 'Group' ) {
+	    $ADDObjects = Invoke-OnDC $AdminCredential { Get-ADGroup -SearchBase $Using:OU -Filter $Using:Filter } |
+            Sort-Object -Property Name
+    } ElseIf( $ObjectType -eq 'Policies' ) {
+	    $ADDObjects = Invoke-OnDC $AdminCredential { Get-GPO -All }
+    } ElseIf( $ObjectType -eq 'PolicyReg' ) {
+        Get-GPRegistryValue -Guid $Filter `
+            -Key "HKCU\Software\Policies\Google\Chrome" `
+            -ValueName "ManagedBookmarks" -ErrorAction SilentlyContinue
+    }
+
+    Return ,$ADDObjects
+}
